f80251ecbaa865decd25358678e462f5f5d2c7a2
SobresitoDeClorazepan <124885618+SobresitoDeClorazepan@users.noreply.github.com>
2023-02-22 23:36:54 -0500

    Update característica de ES6.js

--- característica de ES6.js
+++ característica de ES6.js
@@ -1,1 +1,9 @@
+const nombre = "Pepito"
+const apellido = "Perez"
 
+console.log(nombre + " " + apellido);
+console.log(`${nombre} ${apellido}`)
+
+function componente({ backgroundColor }) {
+  return (<div className={
+  
