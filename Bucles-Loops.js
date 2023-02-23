517da0f1a8bee30e42ca40f118d00d4002a85301
SebasLeon1 <124885452+SebasLeon1@users.noreply.github.com>
2023-02-23 00:37:30 -0500

    Update Bucles-Loops.js

--- Bucles-Loops.js
+++ Bucles-Loops.js
@@ -1,1 +1,21 @@
-hola
+function bucleWhile(num) {
+let i = 0;
+while(i < num) {
+console.log(i);
+i++;
+}
+}
+
+bucleWhile(11);
+
+
+function doWhile(num) {
+let i = 0;
+do {
+console.log(i);
+i**;
+} while(i< num);
+}
+
+doWhile(0);
+
